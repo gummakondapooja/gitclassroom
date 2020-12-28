905fb013a10aa98410f866ad895acbfac5a416d4
gummakondapooja <pooja.gummakonda32@gmail.com>
2020-12-28 19:20:05 +0530

    java program 3

---
+++ Prime.java
@@ -0,0 +1,22 @@
+public class Prime
+{
+public static void main(String[] args)
+{
+  int n = 4,count=0;
+  for(int i=1; i<=n;i++)
+{
+   if (n%i == 0)
+{
+   count++;
+}
+}
+if(count==2)
+{
+System.out.println("given number is prime");
+}
+else
+{
+System.out.println("given number is not a prime");
+}
+}
+}
