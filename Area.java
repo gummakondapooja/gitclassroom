0a85783850a49752a4c48930174ed849d4689388
gummakondapooja <pooja.gummakonda32@gmail.com>
2020-12-28 18:57:34 +0530

    java program 1 added

---
+++ Area.java
@@ -0,0 +1,11 @@
+
+class Area {
+   public static void main (String[] args)
+   {
+	  
+	   double length = 5;
+	   double width = 10;
+	   double area = length*width;
+	   System.out.println("Area of Rectangle is:"+area);
+   }
+}
